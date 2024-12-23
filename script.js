4d5920dde865b5c7bdb3df865c1ca561a3254e4e
Sashriya <140697894+Sashriya@users.noreply.github.com>
2024-12-23 15:35:40 +0530

    added javascript file

---
+++ script.js
@@ -0,0 +1,7 @@
+// Highlight Active Navigation Link
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function () {
+        document.querySelectorAll('.nav-links a').forEach(el => el.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
